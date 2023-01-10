9e66c925df3ced6c47f2897cb6bdce468e3e656e
Kleber Nascimento <klebernascimento@outlook.com.br>
2023-01-10 19:59:20 -0300

    Aula10

---
+++ aula10/webscraping/web.rb
@@ -0,0 +1,17 @@
+require 'nokogiri'
+require 'net/http'
+
+https = Net::HTTP.new('example.com', 443)
+https.use_ssl = true
+
+response = https.get('/')
+
+doc = Nokogiri::HTML(response.body)
+
+h1 = doc.at('h1')
+
+puts h1.content
+
+doc.search('p').each do |a|
+  puts a.content
+end
